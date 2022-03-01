81a81bfcab28ccc0fef8dbe5d3921c1610c10942
Jangwook Ryu <rxjw95@gmail.com>
2022-03-01 20:02:32 +0900

    [enum]
    - enum 다중 값 만들기
    - getter 만들기

--- src/ch13/enums/EnumPractice.java
+++ src/ch13/enums/EnumPractice.java
@@ -1,27 +1,37 @@
 package ch13.enums;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class EnumPractice {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(PATH.values()));
-        System.out.println(Arrays.toString(Arrays.stream(PATH.values()).map(PATH::getUrl).toArray(String[]::new)));
+        System.out.println(PATH.GOOGLE.getValues()); // [https://www.goggle.com, 구글]
     }
 }
 
 enum PATH {
-    NAVER("https://www.naver.com"),
-    GOOGLE("https://www.goggle.com"),
-    DAUM("https://www.daum.net");
+    NAVER("https://www.naver.com", "네이버"),
+    GOOGLE("https://www.goggle.com", "구글"),
+    DAUM("https://www.daum.net", "다음");
 
     private final String url;
+    private final String koreanDomain;
 
-    PATH(String url) {
+    PATH(String url, String koreanDomain) {
         this.url = url;
+        this.koreanDomain = koreanDomain;
     }
 
     String getUrl() {
         return url;
+    }
+
+    String getKoreanDomain() {
+        return koreanDomain;
+    }
+
+    List<String> getValues() {
+        return List.of(url, koreanDomain);
     }
 
 }
